f7fd1993400bcbf084a19fab4935591f76740b75
dlshnkLK <dilshankaliyanage@yahoo.com>
2023-02-03 03:13:22 +0530

    propertyCharacteristic model added

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Property extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'broker_id',
+        'address',
+        'listing_type',
+        'city',
+        'zip_code',
+        'description',
+        'build_year',
+    ];
 }
