4abb92cc849f63a5c905dc250c45d46fb64da5f6
dlshnkLK <dilshankaliyanage@yahoo.com>
2023-02-04 04:14:44 +0530

    broker controller added

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
+
 //protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
